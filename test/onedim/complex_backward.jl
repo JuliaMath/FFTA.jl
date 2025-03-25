db909bee333ea91de8888b6190a9591c6b92230c
Andreas Noack <andreas@noack.dk>
2025-03-25 12:15:39 +0100

    Add some allocation tests. Bump requirement to Julia 1.9 to have access to @allocations macro.

--- test/onedim/complex_backward.jl
+++ test/onedim/complex_backward.jl
@@ -1,18 +1,23 @@
 using FFTA, Test
-test_nums = [8, 11, 15, 16, 27, 100]
-@testset "backward" begin
-    for N in test_nums
-        x = ones(ComplexF64, N)
-        y = bfft(x)
-        y_ref = 0*y
-        y_ref[1] = N
-        @test y ≈ y_ref atol=1e-12
-    end
+
+@testset "backward. N=$N" for N in [8, 11, 15, 16, 27, 100]
+    x = ones(ComplexF64, N)
+    y = bfft(x)
+    y_ref = 0*y
+    y_ref[1] = N
+    @test y ≈ y_ref atol=1e-12
 end
 
-@testset verbose = true "against naive implementation. Size: $n" for n in 1:64
+@testset "More backward tests. Size: $n" for n in 1:64
     x = complex.(randn(n), randn(n))
-    @test naive_1d_fourier_transform(x, FFTA.FFT_BACKWARD) ≈ bfft(x)
+
+    @testset "against naive implementation" begin
+        @test naive_1d_fourier_transform(x, FFTA.FFT_BACKWARD) ≈ bfft(x)
+    end
+
+    @testset "allocation" begin
+        @test (@allocations bfft(x)) <= 44
+    end
 end
 
 @testset "error messages" begin
