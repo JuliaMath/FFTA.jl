c5f7b892a2d9bfe4f829c37b48e2693482eb6118
Justin Ackers <justin.ackers@imte.fraunhofer.de>
2025-09-02 14:03:22 +0200

    add tests

--- test/twodim/real_forward.jl
+++ test/twodim/real_forward.jl
@@ -6,6 +6,11 @@ using FFTA, Test
     y_ref = 0*y
     y_ref[1] = length(x)
     @test y ≈ y_ref
+    x = randn(N,N)
+    @test rfft(x) ≈ rfft(reshape(x,1,N,N), [2,3])[1,:,:]
+    @test rfft(x) ≈ rfft(reshape(x,1,N,N,1), [2,3])[1,:,:,1]
+    @test rfft(x) ≈ rfft(reshape(x,1,1,N,N,1), [3,4])[1,1,:,:,1]
+    @test size(rfft(x)) == (N÷2+1, N)
 end
 
 @testset "allocations" begin
