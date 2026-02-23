4bf9b1673e5365534419565d939b6045f2741958
wheeheee <104880306+wheeheee@users.noreply.github.com>
2026-02-23 14:39:36 +0800

    add tests for exceptions

--- test/argument_checking.jl
+++ test/argument_checking.jl
@@ -2,10 +2,9 @@ using Test, FFTA
 using LinearAlgebra: LinearAlgebra
 
 @testset "Only 1D and 2D FFTs" begin
-    xr = zeros(2, 2)
+    xr = zeros(2, 2, 2)
     xc = complex(xr)
-    # @test_throws ArgumentError("only supports 1D and 2D FFTs") plan_fft(xc, 1:3)
-    # @test_throws ArgumentError("only supports 1D and 2D FFTs") plan_bfft(xc, 1:3)
+
     @test_throws ArgumentError("only supports 1D and 2D FFTs") plan_rfft(xr, 1:3)
     @test_throws ArgumentError("only supports 1D and 2D FFTs") plan_brfft(xc, 2, 1:3)
 end
@@ -59,26 +58,41 @@ end
             @test_throws DimensionMismatch plan_brfft(yr2, size(xr2, 1)) * yr2p
         end
     end
+    @testset "3D array" begin
+        xc3 = randn(ComplexF64, 3, 3, 3)
+        yc3 = randn(ComplexF64, 5, 5, 5)
+        pxc3 = plan_fft(xc3)
+        @test_throws DimensionMismatch pxc3 * yc3
+        invalid_p = plan_fft(randn(ComplexF64, ntuple(i -> 3, 5)), 3:5)
+        xc4 = randn(ComplexF64, (1, ntuple(i -> 5, 3)...))
+
+        ### plan region out of bounds
+
+        # all same dims
+        @test_throws DimensionMismatch("Plan region is outside array dimensions.") invalid_p * xc3
+        # dim(p) < dim(out) = dim(in)
+        @test_throws DimensionMismatch("Plan region is outside array dimensions.") LinearAlgebra.mul!(xc4, invalid_p, xc4)
+    end
 end
 
 @testset "mismatch between input and output arrays" begin
     @testset "1D plan 1D array" begin
-        x1 = complex(randn(3))
+        x1 = randn(ComplexF64, 3)
         y1 = similar(x1, length(x1) + 1)
 
         @test_throws DimensionMismatch LinearAlgebra.mul!(y1, plan_fft(x1), x1)
     end
 
-    @testset "2D array" begin
-        x2 = complex.(randn(3, 3), randn(3, 3))
-        y2 = similar(x2, size(x2, 1) + 1, size(x2, 2) + 1)
+    @testset "$(N)D array" for N in 2:3
+        xN = randn(ComplexF64, ntuple(i -> 3, N))
+        yN = similar(xN, size(xN) .+ 1)
 
-        @testset "1D plan, region=$(region)" for region in [1, 2]
-            @test_throws DimensionMismatch LinearAlgebra.mul!(y2, plan_fft(x2, region), x2)
+        @testset "1D plan, region=$(region)" for region in 1:N
+            @test_throws DimensionMismatch LinearAlgebra.mul!(yN, plan_fft(xN, region), xN)
         end
 
-        @testset "2D plan" begin
-            @test_throws DimensionMismatch LinearAlgebra.mul!(y2, plan_fft(x2), x2)
+        @testset "$(N)D plan" begin
+            @test_throws DimensionMismatch LinearAlgebra.mul!(yN, plan_fft(xN), xN)
         end
     end
 end
