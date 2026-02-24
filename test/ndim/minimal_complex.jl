6a6ae19e1d09557c4bd73539f999c585d6180fc1
wheeheee <104880306+wheeheee@users.noreply.github.com>
2026-02-24 23:45:09 +0800

    test nd

---
+++ test/ndim/minimal_complex.jl
@@ -0,0 +1,27 @@
+using FFTA, Test
+
+@testset "Basic ND checks" begin
+    for sz in ((3, 5, 7), (4, 14, 9), (103, 5, 13), (26, 33, 35, 4), ntuple(i -> 3, 5))
+        x = ones(sz)
+        @test fft(x) ≈ setindex!(zeros(sz), prod(sz), 1)
+    end
+
+    y = zeros((3, 3, 3))
+    y[2, 2, 2] = 1
+    w1 = -0.5 - sqrt(3)im / 2
+    w2 = conj(w1)
+    y_ref = ComplexF64[
+        1 w1 w2;
+        w1 w2 1;
+        w2 1 w1
+        ;;;
+        w1 w2 1;
+        w2 1 w1;
+        1 w1 w2
+        ;;;
+        w2 1 w1;
+        1 w1 w2;
+        w1 w2 1
+    ]
+    @test isapprox(fft(y), y_ref)
+end
